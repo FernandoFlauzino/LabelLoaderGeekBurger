a4d3bdac3d134a267d3228a8cb22c790a4437ee2
Fernando Flauzino <fernando.flauzino@atlasproj.com>
2019-05-19 11:40:35 -0300

    console app

---
+++ GeekBurguer.LabelLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeekBurguer.LabelLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
