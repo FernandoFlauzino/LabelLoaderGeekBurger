e58c575a6ec1fc48e2c7a85e9563811f7bf59b40
Fernando Flauzino <fernando.flauzino@atlasproj.com>
2019-06-05 08:50:54 -0300

    rename

---
+++ GeekBurger.LabelLoader.Web/Application/Interface/Api/IIngredientsRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekBurger.Ingredients.Contract.Response;
+using GeekBurger.LabelLoader.Web.Application.Request.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.LabelLoader.Web.Application.Interface.Api
+{
+    public interface IIngredientsRepository
+    {
+        Task<bool> CreateIngredients(IngredientsToUpsert request);
+    }
+}
---
+++ GeekBurger.LabelLoader.Web/Controller/LabelLoaderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeekBurger.LabelLoader.Web.Application.Interface;
+using GeekBurger.LabelLoader.Web.Application.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekBurger.LabelLoader.Web.Controller
+{
+    [Produces("application/json")]
+    [Route("api/LabelLoader")]
+    public class LabelLoaderController : ControllerBase
+    {
+        private readonly ILabelLoaderService _labelLoaderService;
+
+        public LabelLoaderController(ILabelLoaderService labelLoaderService)
+        {
+            _labelLoaderService = labelLoaderService;
+        }
+
+ 
+        [HttpPost]
+        public IActionResult ReadImage([FromBody] ReadRequest request)
+        {
+            try
+            {
+                var result = _labelLoaderService.ReadImageVisonService(request.PathImage).Result;
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
